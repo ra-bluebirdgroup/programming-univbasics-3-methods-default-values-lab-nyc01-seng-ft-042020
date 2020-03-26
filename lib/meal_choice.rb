13a4b534b54e06512c00bfd2e7486468066ae538
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-26 20:57:17 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,7 +3,6 @@
 def meal_choice(veg1, veg2, protein="tofu")
   order = "A plate of #{protein} with #{veg1} and #{veg2}."
   puts "What a nutritious meal!"
-  #puts order
-  #return order
-  p order 
+  puts order
+  return order
 end
